664a0daac3b98fbfed2221697bc74d12e9c345d5
kiszkot <kiszkot@gmail.com>
2022-01-22 10:38:17 +0100

    Better navbar and other pages
    Added a folding side menu on the left on the screen by pressing "Kiszkot TV". Also divided the pages link onto different pages.

--- image.js
+++ image.js
@@ -1,6 +1,7 @@
 var links = ["https://www.youtube.com/embed/HPXzJ_K0d6g","https://www.youtube.com/embed/znOWjJniHpQ"];
 var n = 0;
 var width = screen.width;
+let navOpen = 0;
 
 function next(){
 	var change = document.getElementById("link");
@@ -26,3 +27,28 @@ function getWidth() {
 	var x = String(document.getElementById("width").innerHTML);
 	document.getElementById("width").innerHTML = x + String(width) + 'px';
 }
+
+function openNav() {
+	if(navOpen == 0) {
+		openSide();
+	}
+	else {
+		closeSide();
+	}
+}
+
+	
+
+	function openSide() {
+		document.getElementById("left").style.display = "flex";
+		document.getElementById("left").style.width = "10vw";
+		document.getElementById("left").style.height = "auto";
+		navOpen = 1;
+	}
+
+	function closeSide() {
+		document.getElementById("left").style.display = "none";
+		document.getElementById("left").style.width = "0";
+		document.getElementById("left").style.height = "0";
+		navOpen = 0;
+	}
